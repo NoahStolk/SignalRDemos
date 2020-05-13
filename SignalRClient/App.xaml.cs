33174194cadb124e5ed8689fbabaed1f3bdd7847
Noah Stolk <noah.stolk1997@gmail.com>
2020-05-13 21:08:16 +0200

    Remove WPF client app

--- SignalRClient/App.xaml.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Windows;
-
-namespace SignalRClient
-{
-	public partial class App : Application
-	{
-	}
-}
--- SignalRClient/Code/Constants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SignalRClient.Code
-{
-	public static class Constants
-	{
-		public static string SimpleChatHubUrl = "https://localhost:5001/simple-chat-hub?group_name=test_group";
-	}
-}
