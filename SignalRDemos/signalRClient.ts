5da6799894abe704ab71c51b51aa01e337469ea0
Noah Stolk <noah.stolk1997@gmail.com>
2020-05-18 16:02:52 +0200

    Update signalRClient.ts

--- SignalRDemos/signalRClient.ts
+++ SignalRDemos/signalRClient.ts
@@ -2,7 +2,7 @@
 //     Changes to this file may cause incorrect behavior and will be lost if
 //     the code is regenerated.
 
-export interface IUserSessionConnectionInfo {
+export interface IConnectionInfo {
 	userId: string;
 }
 export interface ISimpleChatClientReceiveColors {
